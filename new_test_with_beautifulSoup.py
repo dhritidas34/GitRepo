aaac9222bbcfc210393b32e9886104842476a1e5
dhriti das <dhriti_das@persistent.co.in>
2019-03-18 15:21:32 +0530

    My first commit

---
+++ new_test_with_beautifulSoup.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+
+# disable HTTPS warnings
+urllib3.disable_warnings()
+
+#EUCTR URL link
+URL = "https://www.clinicaltrialsregister.eu/ctr-search/search?query=cancer"
+
+r  = requests.get(URL,verify=False)
+
+data = r.content
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all("div", class_="results grid_8plus"):
+    print(link.get('href'))
